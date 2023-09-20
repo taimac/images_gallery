d0b1ed00d23aa582e9350938a62753c882e107e5
taimac <45276175+taimac@users.noreply.github.com>
2023-09-19 21:03:37 -0300

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASF_KEY;
+
 const App = () => {
   const [word, setWord] = useState('');
 
-  const handleSearchSubmit = (e) => { // This is utilized for sending API requests.
+  const handleSearchSubmit = (e) => {
+    // This is utilized for sending API requests.
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+        .then((data) =>{
+          console.log(data);
+        })
+         .catch((err) =>{
+          console.log(err);
+         })
   };
 
   return (
